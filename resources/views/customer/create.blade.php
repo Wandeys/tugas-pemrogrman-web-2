60d39d7d42ffb74d75a5e39f71d22837cab05906
Wandeys <wandeysgrosby@gmil.com>
2026-05-18 18:40:59 -0700

    Komponen Layout: penyiapan blade component untuk layout utama

---
+++ resources/views/customer/create.blade.php
@@ -0,0 +1,10 @@
+<x-app>
+
+
+    <x-slot:title>
+        {{ $title }}
+    </x-slot>
+
+
+    <h1 class="fw-bold">Form Create Customer</h1>
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/customer', function () {
+    return view('customer.index', ['title' => 'Customer']);
+});
+
+Route::get('/customer/create', function () {
+    return view('customer.create', ['title' => 'Create Customer']);
+});
