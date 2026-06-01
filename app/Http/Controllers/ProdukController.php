401581137d511a0cd477735c0fef1b8b8989034e
Wandeys <wandeysgrosby@gmil.com>
2026-05-31 18:39:08 -0700

    Fitur Create Produk 2: implementasi form tambah data produk yang terintegrasi dengan pilihan kategori

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kategori;
 use App\Models\Produk;
 use Illuminate\Http\Request;
 
@@ -12,16 +13,16 @@ class ProdukController extends Controller
      */
     public function index()
     {
-    $produks =  Produk::latest();
-    $keyword = request('keyword');
-   if($produks){
-    $produks->where('name','like','%'. $keyword .'%');
-   }
+        $produks = Produk::latest();
+        $keyword = request('keyword');
+        if ($produks) {
+            $produks->where('name', 'like', '%'.$keyword.'%');
+        }
 
- return view('produk.index', [
+        return view('produk.index', [
             'title' => 'Produk',
-            'produks'=>$produks->paginate(15)->withQueryString(),
-            ]);
+            'produks' => $produks->paginate(15)->withQueryString(),
+        ]);
     }
 
     /**
@@ -29,15 +30,40 @@ class ProdukController extends Controller
      */
     public function create()
     {
-        //
+        return view('produk.create', [
+            'title' => 'Create Produk',
+            'kategoris' => Kategori::latest()->get(),
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        //
+      {
+        $validated = $request->validate([
+        'name' => ['required','max:255'],
+        'kategori_id' => ['required','exists:kategoris,id'],
+        'harga' => ['required','numeric','min:0'],
+        'stok' => ['required','integer','min:0'],
+        'bahan' => ['required','max:255'],
+    ],[
+        'name.required'=> 'Nama tidak boleh kosong',
+        'name.max'=> 'Nama tidak boleh lebih dari :max karakter',
+        'kategori_id.required'=> 'Jenis produk tidak boleh kosong',
+        'kategori_id.exists'=> 'Jenis produk tidak valid',
+        'harga.required'=> 'Harga tidak boleh kosong',
+        'harga.numeric'=> 'Harga harus berupa angka',
+        'harga.min'=> 'Harga tidak boleh kurang dari :min',
+        'stok.required'=> 'Stok tidak boleh kosong',
+        'stok.integer'=> 'Stok harus berupa angka',
+        'stok.min'=> 'Stok tidak boleh kurang dari :min',
+        'bahan.required'=> 'Bahan tidak boleh kosong',
+        'bahan.max'=> 'Bahan tidak boleh lebih dari :max karakter',
+    ]);
+ 
+  Produk::create( $validated );
+  return to_route('produk.index')->withSuccess('Produk berhasil ditambahkan');
     }
 
     /**
--- resources/views/produk/create.blade.php
+++ resources/views/produk/create.blade.php
@@ -1,3 +1,74 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-</div>
+<x-app>
+
+
+    <x-slot:title>
+        {{ $title }}
+    </x-slot>
+
+    <form method="POST" action="{{ route('produk.store') }}">
+        @csrf
+
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                name="name"value="{{ old('name') }}">
+            @error('name')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+
+        </div>
+
+        <div class="mb-3">
+            <label for="kategori_id" class="form-label">Kategori</label>
+
+            <select class="form-select" @error('kategori_id') is-invalid
+             @enderror id="kategori_id"
+                name="kategori_id">
+                <option value="">Choose Kategori</option>
+                @foreach ($kategoris as $kategori)
+                    <option value="{{ $kategori->id }}" @selected(old('kategori_id') == $kategori)>
+                        {{ $kategori->name_kategori }}
+                    </option>
+                @endforeach
+            </select>
+
+
+            @error('kategori_id')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="harga" class="form-label">Harga</label>
+            <input type="number" class="form-control @error('harga') is-invalid @enderror" id="harga"
+                name="harga"value="{{ old('harga') }}" step="0.01">
+            @error('harga')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="stok" class="form-label">Stok</label>
+            <input type="number" class="form-control @error('stok') is-invalid @enderror" id="stok"
+                name="stok"value="{{ old('stok') }}" step="1">
+            @error('stok')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="bahan" class="form-label">Bahan</label>
+            <input type="text" class="form-control @error('bahan') is-invalid @enderror" id="bahan"
+                name="bahan"value="{{ old('bahan') }}">
+            @error('bahan')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <a class="btn btn-warning" href="{{ route('produk.index') }}" role="button">Cancel</a>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+
+</x-app>
