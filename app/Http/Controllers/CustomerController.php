860e2a8e91ed775fdba1606507d796577839d3b5
Wandeys <wandeysgrosby@gmil.com>
2026-06-06 19:14:11 -0700

    Update Fitur Create: implementasi database transaction pada store Customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -37,6 +37,7 @@ class CustomerController extends Controller
         'phone' => ['required','numeric'],
         'address' => ['required','max:255'],
         'join_date' => ['required','date'],
+        'gender'=>['required', 'in:Male,Female']
     ],[
         'name.required'=> 'Nama tidak boleh kosong',
         'name.max'=> 'Nama tidak boleh lebih dari :max karakter',
@@ -47,6 +48,9 @@ class CustomerController extends Controller
         'address.required'=> 'Address tidak boleh kosong',
         'join_date.required'=> 'Join Date tidak boleh kosong',
         'join_date.date'=> 'Join Date tidak valid',
+        'gender.required'=>'Gender tidak boleh kosong',
+        'gender.in:Male,Female'=>'Pilih salah satu Male atau Female',
+
     ]);
  
   Customer::create( $validated );
